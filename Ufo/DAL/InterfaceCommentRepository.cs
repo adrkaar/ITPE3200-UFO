df45dd73a8d2fd9ec7659a008d979535b7daac87
adrkaar <112952665+adrkaar@users.noreply.github.com>
2022-10-20 18:54:11 +0200

    Formateringa av kode

---
+++ Ufo/DAL/InterfaceCommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ufo.Models;
+
+namespace Ufo.DAL
+{
+    public interface InterfaceCommentRepository
+    {
+        Task<List<Comment>> FetchAllComments();
+        Task<bool> AddComment(Comment inComment);
+        Task<bool> DeleteComment(int id);
+    }
+}
