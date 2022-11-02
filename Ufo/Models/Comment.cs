3d8b3d2fb3777479cdab56f8119ec1f43daf9e8c
Marie Spongsveen <marie.spongsveen@gmail.com>
2022-11-02 19:20:02 +0100

    endret antall for regex

--- Ufo/Models/Comment.cs
+++ Ufo/Models/Comment.cs
@@ -5,7 +5,7 @@ namespace Ufo.Models
     public class Comment
     {
         public int Id { get; set; }
-        [RegularExpression(@"^[a-zA-Z .,-?!]{1,160}$")]
+        [RegularExpression(@"^[a-zA-Z .,-?!]{1,200}$")]
         public string Text { get; set; }
         public int ObservationId { get; set; }
         [RegularExpression(@"^[0-9]{1,3}$")]
--- Ufo/Models/Observation.cs
+++ Ufo/Models/Observation.cs
@@ -10,7 +10,7 @@ namespace Ufo.Models
         public string Date { get; set; }
         //[RegularExpression(@"^$")]
         public string Time { get; set; }
-        [RegularExpression(@"^[a-zA-Z .,-?!]{1,160}$")]
+        [RegularExpression(@"^[a-zA-Z .,-?!]{1,200}$")]
         public string Description { get; set; }
         [RegularExpression(@"^[0-9.-]{1,10}$")]
         public string Latitude { get; set; }
