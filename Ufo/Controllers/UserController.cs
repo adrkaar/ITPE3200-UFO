d475a2ed21e9d905be3408a0e9e0c1995966aa0d
adrkaar <112952665+adrkaar@users.noreply.github.com>
2022-11-02 15:20:49 +0100

    Kontroller for bruker

---
+++ Ufo/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Ufo.DAL;
+using Ufo.Models;
+
+namespace Ufo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private InterfaceUserRepository _db;
+
+        private ILogger<UserController> _log;
+        
+        public async Task<ActionResult> LoggInn(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool returnResult = await _db.Login(user);
+                if (!returnResult)
+                {
+                    _log.LogInformation("Login failed for " + user.Username);
+                    return Ok(false);
+                }
+                return Ok(true);
+            }
+            _log.LogInformation("Error in validation");
+            return BadRequest("Wrong on input validation");
+        }
+    }
+}
