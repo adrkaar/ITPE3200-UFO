ac414299bc6ec93eda98ceb8fd922054c2cf7f36
adrkaar <112952665+adrkaar@users.noreply.github.com>
2022-11-26 23:39:15 +0100

    Skallert maps

--- Ufo/ClientApp/src/app/views/home/home.component.ts
+++ Ufo/ClientApp/src/app/views/home/home.component.ts
@@ -20,6 +20,8 @@ export class HomeComponent implements OnInit {
         this.map = new google.maps.Map(document.getElementById("homeMap") as HTMLElement, {
             center: { lat: 48.647983479154824, lng: 9.865054057063944 },
             zoom: 3,
+            //mapTypeId: google.maps.MapTypeId.SATELLITE,
+            disableDefaultUI: true,
         });
     }
 
