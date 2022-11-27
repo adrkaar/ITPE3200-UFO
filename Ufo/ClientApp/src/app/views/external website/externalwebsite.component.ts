718059129d77a14e2470f4199ee37845f0d78cec
OlgaBykava <72934706+OlgaBykava@users.noreply.github.com>
2022-11-27 22:15:03 +0100

    endringer til side til eksterne kilder

--- Ufo/ClientApp/src/app/views/external website/externalwebsite.component.ts
+++ Ufo/ClientApp/src/app/views/external website/externalwebsite.component.ts
@@ -1,6 +1,7 @@
 ﻿import
 { Component, OnInit }
-from '@angular/core';
+    from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
 templateUrl: 'externalwebsite.component.html',
