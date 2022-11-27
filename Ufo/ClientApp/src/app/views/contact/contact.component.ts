4c18833c2cb7da40dbf02a2f9c4a96b80403eec4
Marie Spongsveen <marie.spongsveen@gmail.com>
2022-11-27 13:53:17 +0100

    jobbet med mailfunksjon

--- Ufo/ClientApp/src/app/views/contact/contact.component.ts
+++ Ufo/ClientApp/src/app/views/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactMessage } from '../../models/contactMessage.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     templateUrl: 'contact.component.html',
@@ -44,13 +45,18 @@ export class ContactComponent {
     ngOnInit(): void {
     }
 
-    constructor(private http: HttpClient, private router: Router, private formBuilder: FormBuilder) {
+    constructor(private http: HttpClient, private router: Router, private formBuilder: FormBuilder, private sanitizer: DomSanitizer) {
         this.ContactForm = formBuilder.group(this.validation);
     }
 
+    mailtoHeader = "mailto:?";
+    subjectProp = "subject=";
+    bodyProp = "body=";
+
     contact() {
-        this.ContactForm.reset();
-        alert("Thanks for contacting us!");
+        /* https://stackblitz.com/edit/mailto-links-for-assets?file=src%2Fapp%2Fmailto-example%2Fmailto-example.component.ts */
+        const url = `${this.mailtoHeader}${this.subjectProp}${this.newContactMessage.name}&${this.bodyProp}${this.newContactMessage.message}`;
+        return this.sanitizer.bypassSecurityTrustUrl(url)
 
         this.http.post<ContactMessage>('api/contact/handleContact', this.newContactMessage)
             .subscribe(() => {
