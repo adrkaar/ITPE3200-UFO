651778d9400a971d8e6679054f20716ca07a6a44
Marie Spongsveen <marie.spongsveen@gmail.com>
2022-11-28 16:53:59 +0100

    ryddet

--- Ufo/ClientApp/src/app/app.module.ts
+++ Ufo/ClientApp/src/app/app.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { CommentComponent } from './views/comment/comment.component';
 import { HomeComponent } from './views/home/home.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
@@ -14,44 +13,44 @@ import { AddObservationComponent } from './views/observation/addObservation.comp
 import { EditObservationComponent } from './views/observation/editObservation.component';
 import { ObservationComponent } from './views/observation/observation.component';
 
-
-/*Material*/
+/* Material */
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-/*Pipe*/
+/* Pipe */
 import { SafeHtml } from './pipe';
 
-/*Maps*/
+/* Maps */
 import { GoogleMapsModule } from '@angular/google-maps';
 import { ContactComponent } from './views/contact/contact.component';
 import { Router, RouterModule } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+
+/* Modal */
 import { DialogComponent } from './components/dialog/dialog.component';
 import { WarningmodalComponent } from './components/dialog/warningmodal/warningmodal.component';
 import { WelcomemodalComponent } from './components/dialog/welcomemodal/welcomemodal.component';
 import { LoginmodalComponent } from './components/dialog/loginmodal/loginmodal.component';
-
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         HomeComponent,
-      /*Oversation*/
+        SafeHtml,
+        /* Oversation */
         ObservationComponent,
         AddObservationComponent,
         EditObservationComponent,
-      /*Comment*/
+        /* Comment */
         CommentComponent,
-        SafeHtml,
         ContactComponent,
-           /* Login */
+        /* Login */
         LoginComponent,
-      /* Dialog / Modals */ 
+        /* Dialog / Modals */
         DialogComponent,
         WarningmodalComponent,
         WelcomemodalComponent,
@@ -61,7 +60,7 @@ import { LoginmodalComponent } from './components/dialog/loginmodal/loginmodal.c
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
         HttpClientModule,
         FormsModule, ReactiveFormsModule,
-        MatIconModule, MatTooltipModule, 
+        MatIconModule, MatTooltipModule,
         MatButtonModule,
         MatFormFieldModule, MatInputModule,
         GoogleMapsModule,
